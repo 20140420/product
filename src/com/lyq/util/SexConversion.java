6e6499007f384c41018571482320693cc11b54b0
姜泉 <595121869@qq.com>
2014-05-28 16:32:11 +0800

    理解类型转换器后，接下来做订单购物车管理
    理解类型转换器后，接下来做订单购物车管理

--- src/com/lyq/util/SexConversion.java
+++ src/com/lyq/util/SexConversion.java
@@ -12,19 +12,23 @@ import com.lyq.model.Sex;
  * @author Li Yongqiang
  */
 public class SexConversion extends StrutsTypeConverter {
-
+	
+	//如果是对象（数组，日期，对象等都进入这个处理方法）
 	@SuppressWarnings("unchecked")
 	@Override
 	public Object convertFromString(Map arg0, String[] arg1, Class arg2) {
+		//把传入的一串字符串解析成一个对象，然后返回给对应的字段。
 		Sex sexs[] = Sex.values();
 		for (Sex sex : sexs) {
 			if(sex.getName().equals(arg1[0])){
+				System.out.println("OK~~对象处理完成，返回对象");
 				return sex;
 			}
 		}
 		return null;
 	}
-
+	
+	//如果表单name 对应的 java字段  只是字符串
 	@SuppressWarnings("unchecked")
 	@Override
 	public String convertToString(Map arg0, Object arg1) {
